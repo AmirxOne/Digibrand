c27f21b80fa8d4f9b995bdefc7924756c84c25e4
Amir.x.One <Amir.x.one.2000@gmail.com>
2022-06-25 18:26:43 +0430

    create Reducer

---
+++ src/context/ContextCart.js
@@ -0,0 +1,73 @@
+import React, { useReducer } from "react";
+
+export const CartContext = React.CartContext();
+
+const initialState = {
+  selectItem: [],
+  itemCounter: 0,
+  total: 0,
+  chckOut: false,
+};
+
+const cartReducer = (state, action) => {
+  switch (action.type) {
+    case "ADD":
+        if(!state.selectItem.find(item => item.id === action.payload.id )){
+            state.selectItem.push({...action.payload , quntity:1})
+        }
+        return{
+            ...state, selectItem:[...state.selectItem]
+        }
+
+    case "REMOVE":
+        const newSelectedItem = state.selectItem.filter(item => item.id !== action.payload.id);
+        return{
+            ...state,selectItem:[...newSelectedItem]
+        }
+
+    case "UP":
+        const indexUp = state.selectItem.findIndex(item => item.id === action.payload.id);
+        state.selectItem[indexUp].quntity++;
+        return{
+            ...state ,
+        }
+
+    case "DOWN":
+        const indexDown = state.selectItem.findIndex(item => item.id === action.payload.id);
+        state.selectItem[indexDown].quntity--;
+        return{
+            ...state ,
+        } 
+
+    case "CHCKOUT":
+      return {
+        selectItem: [],
+        itemCounter: 0,
+        total: 0,
+        chckOut: true,
+      };
+
+    case "CLEAR":
+      return {
+        selectItem: [],
+        itemCounter: 0,
+        total: 0,
+        chckOut: false,
+      };
+
+    default:
+      return state;
+  }
+};
+
+const ContextCart = ({ children }) => {
+  const [state, dispatch] = useReducer(cartReducer, initialState);
+
+  return (
+    <CartContext.Provider value={{ state, dispatch }}>
+        {children}
+    </CartContext.Provider>
+  );
+};
+
+export default ContextCart;
